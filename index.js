8865fbeef338c2ca8c4007e003a0e4bb9cf5828b
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-09-17 05:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  let newKittens = kittens.slice(0,3);
+  let newKittens = kittens.slice(0);
   return newKittens.push(name);
 }
